1faf1e95a2b9a07d6e52a2a0e928079ef1e81357
PakapolTaubol <pakapol.taubol@mail.kmutt.ac.th>
2021-11-08 23:07:02 +0700

    Drop01

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,4 +1,9 @@
 import Phaser from "phaser";
+import { NONE } from "phaser/src/const";
+
+let mainbg, player;
+let keyA, keyD, keyVis;
+let music,soundVis;
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -8,15 +13,70 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        
+        this.load.image('mainbg', 'src/image/BackG.jpg');
+        this.load.spritesheet('player', 'src/image/player1.png', { frameWidth: 180, frameHeight: 247 });
+        this.load.audio('song','src/image/song/gamesong.mp3');
+        this.load.audio('soundVis','src/image/song/visiblesoundDis.mp3');
     }
 
     create() {
-        
+        //BackG
+        mainbg = this.add.tileSprite(0,0, 1280, 720,'mainbg')
+            .setScale(1.5)
+            .setOrigin(0,0);
+
+        //Player
+        player = this.physics.add.sprite(100, 600, 'player')
+            .setDepth(10)
+            .setScale(0.8);
+
+        //Aims set
+        this.anims.create({
+            key: 'playerrun',
+            frames: this.anims.generateFrameNumbers('player', {
+                start: 0,
+                end: 4
+            }),
+            duration: 500,
+            framerate: 0,
+            repeat: -1
+        })
+        //KEY
+        keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        keyVis = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        //wallblock
+        player.setCollideWorldBounds(true);
+        //music
+        music = this.sound.add('song')
+            .setVolume(0.6)
+            .play({loop: true});
+        soundVis = this.sound.add('soundVis');
     }
 
     update(delta, time) {
+        mainbg.tilePositionX += 4;
+        //Key A D STOP
+        if (keyA.isDown) {player.setVelocityX(-200);}
+        else if (keyD.isDown) {player.setVelocityX(300);}
+        else {player.setVelocityX(0);}
         
+        //Anims
+        if (keyA.isDown) {
+            player.anims.play('playerrun', true);
+        }
+        else if (keyD.isDown) {
+            player.anims.play('playerrun', true);
+        }
+        else {player.anims.play('playerrun', false);}
+        
+        //Visible
+        if (keyVis.isDown) {
+            player.setVisible()
+        } else if (keyVis.isUp) {
+            player.setVisible(1)
+            soundVis.play();
+        } 
     }
 }
 export default GameScene;
---
+++ src/scenes/MainMenu.js
@@ -0,0 +1,41 @@
+import Phaser from "phaser";
+
+let buttonPlay;
+let bg;
+let mainmusic;
+
+class MainMenu extends Phaser.Scene {
+    constructor(test) {
+        super({
+            key: 'MainMenu'
+        });
+    }
+
+    preload() {
+        this.load.image('bg', 'src/image/MainBG.jpg');
+        this.load.image('play', 'src/image/start.png');
+        this.load.audio('mainmusic','src/image/song/mainsong.mp3');
+    }
+
+    create() {
+        //mainmusic = this.sound.add('mainmusic').play();
+        //mainBG
+        bg = this.add.tileSprite(0, 0, 1360, 340, 'bg')
+            .setScale(2)
+            .setOrigin(0, 0);
+
+        //Button
+        buttonPlay = this.add.image(680,340, 'play').setScale(1.5);
+        buttonPlay.setInteractive();
+        buttonPlay.on('pointerup', ()=>{
+            this.scene.start('GameScene')
+        })
+
+        
+    }
+
+    update(delta, time) {
+        bg.tilePositionX += 1;
+    }   
+}
+export default MainMenu;
