4b215f4e76b163c104e3e94b973d4708ef571c2a
tarannaw <wannarat.ppp@mail.kmutt.ac.th>
2021-11-13 15:32:44 +0700

    เพิ่มสกิลปามีดกับspawnมอน

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,7 +1,8 @@
 import Phaser from "phaser";
 
-let mainbg, block, player, enemy1;
-let keyA, keyD, keyW, keyS, keyVis;
+let mainbg, block, player, enemy1, FruitGroup,
+    FruitEvent, fruit, demon, demonGroup, demonSpawn;
+let keyA, keyD, keyW, keyS, keyVis, keyQ;
 let music, soundVis;
 
 class GameScene extends Phaser.Scene {
@@ -14,9 +15,12 @@ class GameScene extends Phaser.Scene {
     preload() {
         this.load.image("mainbg", "src/image/BG.jpg");
         this.load.image("block", "src/image/block2.png");
-        this.load.spritesheet("player", "src/image/player1.png", {
-            frameWidth: 180,
-            frameHeight: 247,
+
+        this.load.image("fruit", "src/image/ninja-fruit.png");
+
+        this.load.spritesheet("player", "src/image/ninja.png", {
+            frameWidth: 229.2,
+            frameHeight: 280,
         });
         this.load.spritesheet("smoke", "src/image/smoke.png", {
             frameWidth: 125.25,
@@ -24,6 +28,12 @@ class GameScene extends Phaser.Scene {
         });
         this.load.audio("song", "src/image/song/gamesong.mp3");
         this.load.audio("soundVis", "src/image/song/visiblesoundDis.mp3");
+
+        this.load.spritesheet("demon", "src/image/demon.png", {
+            frameWidth: 178.75,
+            frameHeight: 185,
+        });
+
         this.load.spritesheet("enemy1", "src/image/player1.png", {
             frameWidth: 180,
             frameHeight: 247,
@@ -80,24 +90,72 @@ class GameScene extends Phaser.Scene {
         });
 
         //Enemy01
-        enemy1 = this.physics.add
-            .sprite(1200, 600, "enemy1")
-            .setDepth(10)
-            .setScale(0.7)
-            .setSize(100, 100)
-            .setOffset(50, 100);
+
+        // demon = this.physics.add
+        //     .sprite(Phaser.Math.Between(600,1100),
+        //             600,'demon')
+        //     // (1200, 600, "demon")
+        //     .setDepth(10)
+        //     .setScale(0.7)
+        //     .setSize(100, 100)
+        //     .setOffset(50, 100)
+        //     .setVelocityX(-100);
 
         //Enemy set
         this.anims.create({
-            key: "enemy1anim",
-            frames: this.anims.generateFrameNumbers("enemy1", {
+            key: "demonanim",
+            frames: this.anims.generateFrameNumbers("demon", {
                 start: 0,
-                end: 4,
+                end: 3,
             }),
             duration: 500,
             framerate: 0,
             repeat: -1,
         });
+
+        demonGroup = this.physics.add.group();
+
+        demonSpawn = this.time.addEvent({
+            delay: 3000,
+            callback: function () {
+                demon = this.physics.add
+                    .sprite(Phaser.Math.Between(900, 1100),
+                        Phaser.Math.Between(400, 600), 'demon')
+                    .setDepth(10)
+                    .setScale(0.7)
+                    .setSize(100, 160)
+                    .setOffset(50, 10);
+
+                demonGroup.add(demon)
+                    .setVelocityX(-100);
+
+
+                demon.anims.play("demonanim", true);
+                demon.flipX = true;
+            },
+            callbackScope: this,
+            loop: true,
+            pause: false
+        });
+
+
+        //Fruit set
+        FruitGroup = this.physics.add.group();
+
+        // FruitEvent = this.time.addEvent({
+        //     delay: 1000,
+        //     callback: function () {
+        //         fruits = this.physics.add.image(fruit.x, fruit.y, 'fruit')
+        //             .setScale(0.2);
+
+        //         FruitGroup.add(fruits);
+
+        //         FruitGroup.setValocityX(-200);
+        //     },
+        //     callbackScope: this,
+        //     loop: true,
+        //     pause: false
+        // });
 
         //KEY
         keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
@@ -106,10 +164,23 @@ class GameScene extends Phaser.Scene {
         keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         keyVis = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
+        keyQ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
+
         //wallblock
         player.setCollideWorldBounds(true);
         this.physics.add.collider(player, block);
         this.physics.add.collider(player, enemy1);
+
+        this.physics.add.collider(player, demonGroup, (player, demon) => {
+            demon.destroy();
+
+        });
+
+        this.physics.add.collider(FruitGroup, demonGroup, (fruit, demon) => {
+            fruit.destroy();
+            demon.destroy();
+        });
+
 
         //music
         music = this.sound.add("song").setVolume(0.1).play({ loop: true });
@@ -120,10 +191,9 @@ class GameScene extends Phaser.Scene {
         mainbg.tilePositionX += 0;
 
         //Enemy1 Anims
-        enemy1.anims.play("enemy1anim", true);
-        enemy1.setVelocityX(0);
-        enemy1.flipX = true;
-        
+        // demon.anims.play("demonanim", true);
+        // demon.flipX = true;
+
         //Key WS STOP
         if (keyS.isDown) {
             player.setVelocityY(200);
@@ -150,12 +220,28 @@ class GameScene extends Phaser.Scene {
         //KeySpace
         if (keyVis.isDown) {
             player.anims.play("smokeVis", true);
-            player.setVelocityX(-100);
+            player.setVelocityX(-50);
         } else if (keyVis.isUp) {
             player.anims.play("playerrun", true);
             //mainbg.tilePositionX += 4;
             soundVis.play();
         }
+
+        //KeyQ
+        if (Phaser.Input.Keyboard.JustDown(keyQ)) {
+
+            fruit = this.physics.add.image(player.x, player.y, 'fruit')
+                .setScale(0.1);
+            // fruit.rotation += 1;
+            FruitGroup.add(fruit);
+            FruitGroup.setVelocityX(200)
+        }
+
+        // this.physics.add.collider(fruit, demon, (fruit, demon)=> {
+
+        //     fruit.destroy();
+        //     demon.destroy();
+        //     });
     }
 }
 export default GameScene;
