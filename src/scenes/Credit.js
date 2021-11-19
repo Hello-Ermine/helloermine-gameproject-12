3cea759f9747f3bd8a9433a6c24553d4ab575897
tarannaw <wannarat.ppp@mail.kmutt.ac.th>
2021-11-19 14:57:13 +0700

    แก้ CREDIT.png to credit.png

--- src/scenes/Credit.js
+++ src/scenes/Credit.js
@@ -12,7 +12,7 @@ class credit extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image("wallcredit", "src/image/CREDIT.png");
+        this.load.image("wallcredit", "src/image/credit.png");
         this.load.image('back', 'src/image/bottom/back.png');
         this.load.audio('selectMenu', 'src/sound/select.mp3');
         this.load.image("bg", "src/image/bgnight02.png");
@@ -24,7 +24,7 @@ class credit extends Phaser.Scene {
         bg = this.add.tileSprite(0, 0, 1280, 720, 'bg')
             .setOrigin(0);
         background = this.add.image(150, 80, 'wallcredit')
-            . setScale(0.8)
+            .setScale(0.8)
             .setOrigin(0);
 
         //-----------------------------ButtonBack-------------------------------//
